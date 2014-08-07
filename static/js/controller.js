47ff1e249da695dd3119dcdfe29a5a3a6ffb91ea
Guillem Borrell <guillemborrell@gmail.com>
2014-08-07 15:39:49 +0200

    Forgot to add the controller

---
+++ static/js/controller.js
@@ -0,0 +1,149 @@
+var taskApp = angular.module('taskApp', ['ngResource']);
+
+taskApp.controller('userController', function($scope, $resource){
+    $scope.userresource = $resource('/REST/user');
+    $scope.subscriptionresource = $resource('/REST/subscription');
+    $scope.groupresource = $resource('/REST/group');
+    $scope.groups = []
+    $scope.subscriptions = [];
+    $scope.users = [];
+    $scope.newusername = '';
+    $scope.newuserpassword = '';
+    $scope.newusersubscription = '';
+    $scope.newusergroup = '';
+    $scope.newsubscriptionname = '';
+    $scope.newgroupname='';
+    $scope.newsubscriptionlevel = 0;
+
+    var datag = $scope.groupresource.get(function(){
+	for (i in datag.data){
+	    $scope.groups.push(datag.data[i]);
+	}
+    });
+    
+
+    var datas = $scope.subscriptionresource.get(function(){
+	for (i in datas.data){
+	    $scope.subscriptions.push(datas.data[i]);
+	}
+    });
+
+						   
+    var datau = $scope.userresource.get(function(){
+	for (i in datau.data){
+	    $scope.users.push(datau.data[i]);
+	}});
+
+    $scope.postSubscription = function(){
+	var data = $scope.subscriptionresource.save(
+	    {"name": $scope.newsubscriptionname,
+	     "level": $scope.newsubscriptionlevel}
+	    )
+	window.location.replace('/users');
+    }
+
+    $scope.postGroup = function(){
+	var data = $scope.groupresource.save(
+	    {"name": $scope.newgroupname}
+	)
+	window.location.replace('/users');
+    }
+
+    $scope.postUser = function(){
+	var data = $scope.userresource.save(
+	    {"name": $scope.newusername,
+	     "password": $scope.newuserpassword,
+	     "group": $scope.newusergroup,
+	     "subscription": $scope.newusersubscription}
+	    );
+	window.location.replace('/users');
+    }
+
+    $scope.deleteUser = function(key){
+	var ask = confirm("¿Seguro que queires borrarlo?");
+	if (ask){
+	    var data = $scope.userresource.remove(params={id: key});
+	    window.location.replace('/users');
+	}
+    }   
+
+    $scope.deleteGroup = function(key){
+	var ask = confirm("¿Seguro que queires borrarlo?");
+	if (ask){
+	    var data = $scope.groupresource.remove(params={id: key});
+	    window.location.replace('/users');
+	}
+    }   
+
+}
+		  );
+
+taskApp.controller('activityController', function($scope, $resource) {
+    $scope.taskresource = $resource("/task");
+
+    $scope.deletetask = function(task){
+	var ask = confirm("¿Seguro que quieres borrar esta tarea?");
+	if (ask){
+	    var data = $scope.taskresource.remove(params={id: task});
+	    window.location.replace('/activity');
+	}
+    }
+});
+
+taskApp.controller('createTask', function ($scope, $resource) {
+    $scope.available_tasks = ['Test','Actividad','Pregunta'];
+    $scope.type = '';
+    $scope.question = {"name": "",
+		       "question": "",
+		       "answer": ""};
+    $scope.activity = {"name":"",
+		       "description":""};
+    $scope.test = {"name": "",
+		   "question_list": [{"question": "",
+				      "choices": ["1", "2", "3", "4"],
+				      "right": 0}
+				    ]
+		  };
+    $scope.error = false;
+
+    $scope.add_question_to_test = function(question_list){
+	question_list.push({"question": "",
+			    "choices": ["", "", "", ""],
+			    "right": 0});
+    }
+    $scope.add_choice_to_question = function(choices){
+	choices.push("");
+    }
+    $scope.remove_choice_from_question = function(choices){
+	choices.pop();
+    }
+    $scope.taskresource = $resource("/task");
+
+    $scope.submit_question = function (){
+	var data = $scope.taskresource.save(
+	    {"kind": $scope.type,
+	     "name": $scope.question.name,
+	     "data": $scope.question}
+	);
+	window.location.replace('/activity');
+    };
+
+    $scope.submit_activity = function (){
+	var data = $scope.taskresource.save(
+	    {"kind": $scope.type,
+	     "name": $scope.activity.name,
+	     "data": $scope.activity}
+	);
+	window.location.replace('/activity');
+    };
+    $scope.submit_test = function (){
+	var data = $scope.taskresource.save(
+	    {"kind": $scope.type,
+	     "name": $scope.test.name,
+	     "data": $scope.test}
+	);
+	window.location.replace('/activity');
+    };
+
+}
+		  );
